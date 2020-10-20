e2aae81158310963390234220f8ded2d89cb61ce
Desale Fentaw <desale.df@gmail.com>
2020-10-20 18:08:23 +0200

    initial commint, setting up everything

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Tensorflow-examples-cnn
+In this repository, I put tensorflow codes that I Wrote when I am training my self of building models using tensorflow
---
+++ lenet-cnn.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+# main lenet class
+class Lenet:
+    def __init__(self):
+        # initialize parameters here
+        self.path = None
+
+
+# execute everything from here
+if __name__ == '__main__':
+    # download mnist dataset
+    (x_train, ytrain), (x_test,)
+
+    print('now everything is running well')
